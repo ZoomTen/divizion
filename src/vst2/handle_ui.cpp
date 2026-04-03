493276b031638be6dcf5bb2a243a1432fed7b782
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-04-03 11:00:29 +0700

    yeah, fuck keyboard input

--- src/vst2/handle_ui.cpp
+++ src/vst2/handle_ui.cpp
@@ -21,13 +21,6 @@ static std::unordered_map<Divizion*, Gui*> effEditInstances = {};
 static bool globalGuiInit = false;
 
 static ImGuiKey ImGui_ImplSDL2_KeycodeToImGuiKey(int keycode);
-static SDL_Scancode VKToSDLScancode(WPARAM vk);
-static LRESULT CALLBACK PluginSubclassProc(HWND hwnd, UINT msg, WPARAM wParam,
-                                           LPARAM lParam, UINT_PTR uIdSubclass,
-                                           DWORD_PTR dwRefData);
-static bool IsAncestor(HWND ancestor, HWND child);
-
-#define WM_RECLAIM_FOCUS (WM_APP + 1)
 
 void handleOpen(Divizion* di, void* ptr)
 {
@@ -54,9 +47,7 @@ void handleOpen(Divizion* di, void* ptr)
     logE("can't init SDL window from own window");
     return;
   }
-  SubclassData* scData = new SubclassData{ win };
   SetFocus(cwindow);
-  SetWindowSubclass(cwindow, PluginSubclassProc, 1, (DWORD_PTR)scData);
   DivizionActionsImpl* i = new DivizionActionsImpl(di->e);
   Gui* g = new Gui(win, i);
   effEditInstances[di] = g;
@@ -145,15 +136,6 @@ void handleClose(Divizion* di)
   if (!effEditInstances.count(di)) return;
 
   SDL_Window* w = effEditInstances[di]->w;
-  SDL_SysWMinfo wmInfo;
-  SDL_VERSION(&wmInfo.version);
-  if (SDL_GetWindowWMInfo(w, &wmInfo)) {
-    HWND hwnd = wmInfo.info.win.window;
-    DWORD_PTR scData = 0;
-    GetWindowSubclass(hwnd, PluginSubclassProc, 1, &scData);
-    RemoveWindowSubclass(hwnd, PluginSubclassProc, 1);
-    delete reinterpret_cast<SubclassData*>(scData);
-  }
   delete effEditInstances[di];
   effEditInstances.erase(di);
   SDL_DestroyWindow(w);
@@ -375,217 +357,4 @@ ImGuiKey ImGui_ImplSDL2_KeycodeToImGuiKey(int keycode)
     return ImGuiKey_F12;
   }
   return ImGuiKey_None;
-}
-
-// this bullshit for solving the key focus because VST windows are weird
-LRESULT CALLBACK PluginSubclassProc(HWND hwnd, UINT msg, WPARAM wParam,
-                                    LPARAM lParam, UINT_PTR uIdSubclass,
-                                    DWORD_PTR dwRefData)
-{
-  SubclassData* data = reinterpret_cast<SubclassData*>(dwRefData);
-
-  switch (msg) {
-  case WM_SETFOCUS:
-    logI("WM_SETFOCUS: gaining=%p hasFocus=%d", (void*)wParam, data->hasFocus);
-    break;
-  case WM_KILLFOCUS: {
-    HWND gaining = (HWND)wParam;
-    logI("WM_KILLFOCUS: gaining=%p isAncestor=%d hasFocus=%d", (void*)gaining,
-         gaining ? IsAncestor(gaining, hwnd) : -1, data->hasFocus);
-    break;
-  }
-  case WM_KEYDOWN:
-  case WM_SYSKEYDOWN:
-    logI("WM_KEYDOWN: vk=%llu repeat=%d", wParam, (lParam >> 30) & 1);
-    break;
-  }
-  uint32_t windowID = SDL_GetWindowID(data->sdlWindow);
-
-  switch (msg) {
-  case WM_KEYDOWN:
-  case WM_SYSKEYDOWN:
-  case WM_KEYUP:
-    logI("WM_KEYUP: vk=%llu", wParam);
-  case WM_SYSKEYUP: {
-    if (wParam == VK_LWIN || wParam == VK_RWIN) break;
-    SDL_Event e = {};
-    e.type =
-      (msg == WM_KEYDOWN || msg == WM_SYSKEYDOWN) ? SDL_KEYDOWN : SDL_KEYUP;
-    e.key.windowID = windowID;
-    e.key.state = (e.type == SDL_KEYDOWN) ? SDL_PRESSED : SDL_RELEASED;
-    e.key.repeat = (e.type == SDL_KEYDOWN && (lParam & (1 << 30))) ? 1 : 0;
-    e.key.keysym.scancode = VKToSDLScancode(wParam); // wParam = VK code
-    e.key.keysym.sym = SDL_GetKeyFromScancode(e.key.keysym.scancode);
-    e.key.keysym.mod = (Uint16)SDL_GetModState();
-    SDL_PushEvent(&e);
-    return 0;
-  }
-  case WM_CHAR:
-  case WM_SYSCHAR: {
-    if (wParam < 0x20) break; // skip control characters
-    wchar_t wch = (wchar_t)wParam;
-    char utf8[5] = {};
-    WideCharToMultiByte(CP_UTF8, 0, &wch, 1, utf8, sizeof(utf8) - 1, nullptr,
-                        nullptr);
-
-    SDL_Event e = {};
-    e.type = SDL_TEXTINPUT;
-    e.text.windowID = windowID;
-    SDL_strlcpy(e.text.text, utf8, sizeof(e.text.text));
-    SDL_PushEvent(&e);
-    return 0;
-  }
-  case WM_SETFOCUS: {
-    if (!data->hasFocus) {
-      data->hasFocus = true;
-      SDL_Event e = {};
-      e.type = SDL_WINDOWEVENT;
-      e.window.windowID = windowID;
-      e.window.event = SDL_WINDOWEVENT_FOCUS_GAINED;
-      SDL_PushEvent(&e);
-    }
-    break;
-  }
-
-  case WM_KILLFOCUS: {
-    HWND gaining = (HWND)wParam;
-    if (gaining == nullptr) {
-      if (data->hasFocus) // only reclaim if we legitimately held focus
-        PostMessage(hwnd, WM_RECLAIM_FOCUS, 0, 0);
-      break; // never push focus-lost SDL event for nil gaining
-    }
-    bool dawReclaiming = IsAncestor(gaining, hwnd);
-    if (!dawReclaiming && data->hasFocus) {
-      data->hasFocus = false;
-      SDL_Event e = {};
-      e.type = SDL_WINDOWEVENT;
-      e.window.windowID = windowID;
-      e.window.event = SDL_WINDOWEVENT_FOCUS_LOST;
-      SDL_PushEvent(&e);
-    }
-    break;
-  }
-  case WM_RECLAIM_FOCUS:
-    SetFocus(hwnd);
-    break;
-  }
-  return DefSubclassProc(hwnd, msg, wParam, lParam);
-}
-
-SDL_Scancode VKToSDLScancode(WPARAM vk)
-{
-  switch (vk) {
-  case VK_BACK:
-    return SDL_SCANCODE_BACKSPACE;
-  case VK_TAB:
-    return SDL_SCANCODE_TAB;
-  case VK_RETURN:
-    return SDL_SCANCODE_RETURN;
-  case VK_ESCAPE:
-    return SDL_SCANCODE_ESCAPE;
-  case VK_SPACE:
-    return SDL_SCANCODE_SPACE;
-  case VK_PRIOR:
-    return SDL_SCANCODE_PAGEUP;
-  case VK_NEXT:
-    return SDL_SCANCODE_PAGEDOWN;
-  case VK_END:
-    return SDL_SCANCODE_END;
-  case VK_HOME:
-    return SDL_SCANCODE_HOME;
-  case VK_LEFT:
-    return SDL_SCANCODE_LEFT;
-  case VK_UP:
-    return SDL_SCANCODE_UP;
-  case VK_RIGHT:
-    return SDL_SCANCODE_RIGHT;
-  case VK_DOWN:
-    return SDL_SCANCODE_DOWN;
-  case VK_INSERT:
-    return SDL_SCANCODE_INSERT;
-  case VK_DELETE:
-    return SDL_SCANCODE_DELETE;
-  case VK_LSHIFT:
-    return SDL_SCANCODE_LSHIFT;
-  case VK_RSHIFT:
-    return SDL_SCANCODE_RSHIFT;
-  case VK_LCONTROL:
-    return SDL_SCANCODE_LCTRL;
-  case VK_RCONTROL:
-    return SDL_SCANCODE_RCTRL;
-  case VK_LMENU:
-    return SDL_SCANCODE_LALT;
-  case VK_RMENU:
-    return SDL_SCANCODE_RALT;
-  case VK_LWIN:
-    return SDL_SCANCODE_LGUI;
-  case VK_RWIN:
-    return SDL_SCANCODE_RGUI;
-  case VK_CAPITAL:
-    return SDL_SCANCODE_CAPSLOCK;
-  case VK_F1:
-    return SDL_SCANCODE_F1;
-  case VK_F2:
-    return SDL_SCANCODE_F2;
-  case VK_F3:
-    return SDL_SCANCODE_F3;
-  case VK_F4:
-    return SDL_SCANCODE_F4;
-  case VK_F5:
-    return SDL_SCANCODE_F5;
-  case VK_F6:
-    return SDL_SCANCODE_F6;
-  case VK_F7:
-    return SDL_SCANCODE_F7;
-  case VK_F8:
-    return SDL_SCANCODE_F8;
-  case VK_F9:
-    return SDL_SCANCODE_F9;
-  case VK_F10:
-    return SDL_SCANCODE_F10;
-  case VK_F11:
-    return SDL_SCANCODE_F11;
-  case VK_F12:
-    return SDL_SCANCODE_F12;
-  case VK_OEM_1:
-    return SDL_SCANCODE_SEMICOLON;
-  case VK_OEM_PLUS:
-    return SDL_SCANCODE_EQUALS;
-  case VK_OEM_COMMA:
-    return SDL_SCANCODE_COMMA;
-  case VK_OEM_MINUS:
-    return SDL_SCANCODE_MINUS;
-  case VK_OEM_PERIOD:
-    return SDL_SCANCODE_PERIOD;
-  case VK_OEM_2:
-    return SDL_SCANCODE_SLASH;
-  case VK_OEM_3:
-    return SDL_SCANCODE_GRAVE;
-  case VK_OEM_4:
-    return SDL_SCANCODE_LEFTBRACKET;
-  case VK_OEM_5:
-    return SDL_SCANCODE_BACKSLASH;
-  case VK_OEM_6:
-    return SDL_SCANCODE_RIGHTBRACKET;
-  case VK_OEM_7:
-    return SDL_SCANCODE_APOSTROPHE;
-  default:
-    // A–Z: VK codes 65–90 map linearly to SDL_SCANCODE_A–Z (4–29)
-    if (vk >= 'A' && vk <= 'Z')
-      return (SDL_Scancode)(SDL_SCANCODE_A + (vk - 'A'));
-    // 0–9: VK codes 48–57
-    if (vk >= '0' && vk <= '9')
-      return (SDL_Scancode)(SDL_SCANCODE_0 + (vk - '0'));
-    return SDL_SCANCODE_UNKNOWN;
-  }
-}
-
-bool IsAncestor(HWND ancestor, HWND child)
-{
-  HWND cur = GetParent(child);
-  while (cur != nullptr) {
-    if (cur == ancestor) return true;
-    cur = GetParent(cur);
-  }
-  return false;
 }
