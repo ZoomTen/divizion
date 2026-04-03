426ade57ec2f221e26843a0d49a5a1b4f7a9c195
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2026-04-03 16:45:38 +0700

    save/load fxb

--- src/vst2/io.cpp
+++ src/vst2/io.cpp
@@ -1,30 +1,25 @@
-#include "src/ta-log.h"
+#include "src/divizion.hpp"
+#include "src/engine/engine.h"
+#include "src/engine/safeWriter.h"
 #include <cstdio>
 #include <cstring>
 #include "vst.hpp"
 
 int32_t setupPresetCopyToHost(Vst::AEffect *effect, void **dest, bool isAPreset)
 {
-  // DivizionInstance *di = (DivizionInstance *)effect->object;
-  // if (!di) return 0;
+  Divizion *di = (Divizion *)effect->object;
+  if (!di) return 0;
 
-  // logV("setupPresetCopyToHost dest:%p, preset?%d", (void*)dest,isAPreset);
-  // memset(di->state, 0, sizeof(di->state));
-  // sprintf(di->state, isAPreset?"preset!":"bank!");
-
-  // *dest = di->state;
-  // logV("setup done");
-  // return sizeof(di->state);
-  return 0;
+  SafeWriter *w = di->e->saveFur(false, true);
+  *dest = w->getFinalBuf();
+  return w->size();
 }
 
 int32_t setupPresetLoadFromHost(Vst::AEffect *effect, void *source, size_t size, bool isAPreset)
 {
-  // DivizionInstance *di = (DivizionInstance *)effect->object;
-  // if (!di) return 0;
-  
-  // logV("setupPresetLoadFromHost source:%p, size:%d, preset?%d", source,size,isAPreset);
-  // memcpy(di->state, source, size>sizeof(di->state)?sizeof(di->state):size);
-  // logV("load done, state is: %s", di->state);
+  Divizion *di = (Divizion *)effect->object;
+  if (!di) return 0;
+
+  di->e->loadFur((unsigned char *)source, size);
   return 1;
 }
